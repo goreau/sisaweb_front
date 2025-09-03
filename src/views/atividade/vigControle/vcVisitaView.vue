e7eb5f9b320a4d1400d3b192575dbb0e293fa43d
goreau <goreau@terra.com.br>
2025-09-03 06:36:47 -0300

    fix: digita cc linha

--- src/views/atividade/vigControle/vcVisitaView.vue
+++ src/views/atividade/vigControle/vcVisitaView.vue
@@ -100,56 +100,95 @@
                 </div>
               </div>
             </div>
-            <div class="columns">
-              <div class="column is-4">
-                <div class="content">
-                  <label class="label">Área</label>
-                  <div class="control">
-                    <CmbGeneric
-                      :sel="vc_linha.id_area"
-                      :data="areas"
-                      @selGen="vc_linha.id_area = $event"
-                      :errclass="{ 'is-danger': v$.id_area.$error }"
-                    />
-                    <span class="is-error" v-if="v$.id_area.$error">
-                      {{ v$.id_area.$errors[0].$message }}
-                    </span>
+            <section v-show="!navs">
+              <div class="columns">
+                <div class="column is-4">
+                  <div class="content">
+                    <label class="label">Área</label>
+                    <div class="control">
+                      <CmbGeneric
+                        :sel="vc_linha.id_area"
+                        :data="areas"
+                        @selGen="vc_linha.id_area = $event"
+                        :errclass="{ 'is-danger': v$.id_area.$error }"
+                      />
+                      <span class="is-error" v-if="v$.id_area.$error">
+                        {{ v$.id_area.$errors[0].$message }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="column is-4">
+                  <div class="content">
+                    <label class="label">Censitário</label>
+                    <div class="control">
+                      <CmbGeneric
+                        :sel="vc_linha.id_censitario"
+                        :data="censitarios"
+                        @selGen="vc_linha.id_censitario = $event"
+                        :errclass="{ 'is-danger': v$.id_censitario.$error }"
+                      />
+                      <span class="is-error" v-if="v$.id_censitario.$error">
+                        {{ v$.id_censitario.$errors[0].$message }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="column is-4">
+                  <div class="content">
+                    <label class="label">Quarteirão</label>
+                    <div class="control">
+                      <CmbGeneric
+                        :sel="vc_linha.id_quarteirao"
+                        :data="quarteiraos"
+                        @selGen="vc_linha.id_quarteirao = $event"
+                        :errclass="{ 'is-danger': v$.id_quarteirao.$error }"
+                      />
+                      <span class="is-error" v-if="v$.id_quarteirao.$error">
+                        {{ v$.id_quarteirao.$errors[0].$message }}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="column is-4">
-                <div class="content">
-                  <label class="label">Censitário</label>
-                  <div class="control">
-                    <CmbGeneric
-                      :sel="vc_linha.id_censitario"
-                      :data="censitarios"
-                      @selGen="vc_linha.id_censitario = $event"
-                      :errclass="{ 'is-danger': v$.id_censitario.$error }"
-                    />
-                    <span class="is-error" v-if="v$.id_censitario.$error">
-                      {{ v$.id_censitario.$errors[0].$message }}
-                    </span>
+            </section>
+            <section v-show="navs">
+              <div class="columns">
+                <div class="column is-6">
+                  <div class="content">
+                    <label class="label">Área</label>
+                    <div class="control">
+                      <CmbGeneric
+                        :sel="vc_linha.id_area_nav"
+                        :data="areas_nav"
+                        @selGen="vc_linha.id_area_nav = $event"
+                        :errclass="{ 'is-danger': v$.id_area_nav.$error }"
+                      />
+                      <span class="is-error" v-if="v$.id_area_nav.$error">
+                        {{ v$.id_area_nav.$errors[0].$message }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="column is-4">
+                  <div class="content">
+                    <label class="label">Quarteirão</label>
+                    <div class="control">
+                      <CmbGeneric
+                        :sel="vc_linha.id_quarteirao"
+                        :data="quarteiraos"
+                        @selGen="vc_linha.id_quarteirao = $event"
+                        :errclass="{ 'is-danger': v$.id_quarteirao.$error }"
+                      />
+                      <span class="is-error" v-if="v$.id_quarteirao.$error">
+                        {{ v$.id_quarteirao.$errors[0].$message }}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="column is-4">
-                <div class="content">
-                  <label class="label">Quarteirão</label>
-                  <div class="control">
-                    <CmbGeneric
-                      :sel="vc_linha.id_quarteirao"
-                      :data="quarteiraos"
-                      @selGen="vc_linha.id_quarteirao = $event"
-                      :errclass="{ 'is-danger': v$.id_quarteirao.$error }"
-                    />
-                    <span class="is-error" v-if="v$.id_quarteirao.$error">
-                      {{ v$.id_quarteirao.$errors[0].$message }}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </section>
             <hr />
             <div class="columns">
               <div class="column">
@@ -267,12 +306,13 @@ import auxiliarService from '@/services/general/auxiliar.service'
 import areaService from '@/services/cadastro/area.service'
 import censitarioService from '@/services/cadastro/censitario.service'
 import quarteiraoService from '@/services/cadastro/quarteirao.service'
+import areaNavService from '@/services/cadastro/areaNav.service'
 import RadioGeneric from '@/components/forms/RadioGeneric.vue'
 import useValidate from '@vuelidate/core'
 import CmbTerritorio from '@/components/forms/CmbTerritorio.vue'
 import CmbGeneric from '@/components/forms/CmbGeneric.vue'
 import DatePicker from '@/components/forms/MyDatePicker.vue'
-import { required$, combo$ } from '@/components/forms/validators'
+import { required$, combo$, requiredIf$ } from '@/components/forms/validators'
 import { ref, onMounted, reactive, watch, computed } from 'vue'
 import { useCurrentUser } from '@/composables/currentUser'
 import { useVcVisitaStore } from '@/stores/vcVisitaStore'
@@ -306,6 +346,9 @@ var areas = ref([])
 var censitarios = ref([])
 var quarteiraos = ref([])
 
+var areas_nav = ref([])
+var navs = ref(false)
+
 var id_prop = ref(0)
 
 var vc_linha = reactive({
@@ -319,6 +362,7 @@ var vc_linha = reactive({
   id_area: 0,
   id_censitario: 0,
   id_quarteirao: 0,
+  id_area_nav: 0,
   id_prod_focal: 0,
   id_prod_peri: 0,
   id_prod_neb: 0,
@@ -342,8 +386,9 @@ const rules = {
   id_tipo: { required$, minValue: combo$(1) },
   id_execucao: { required$, minValue: combo$(1) },
   id_atividade: { required$, minValue: combo$(1) },
-  id_area: { required$, minValue: combo$(1) },
-  id_censitario: { required$, minValue: combo$(1) },
+  id_area_nav: { requiredIf: requiredIf$(() => navs.value === true) },
+  id_area: { requiredIf: requiredIf$(() => navs.value === false) },
+  id_censitario: { requiredIf: requiredIf$(() => navs.value === false) },
   id_quarteirao: { required$, minValue: combo$(1) },
   id_prod_focal: { required$, minValue: combo$(1) },
   id_prod_peri: { required$, minValue: combo$(1) },
@@ -353,6 +398,28 @@ const rules = {
 }
 
 const v$ = useValidate(rules, vc_linha)
+
+const filtro = computed(() => [vc_linha.id_atividade, vc_linha.id_municipio])
+
+watch(filtro, async ([novo1, novo2]) => {
+  if (novo1 && novo2) {
+    if (novo1 == 6 || novo1 == 7) {
+      navs.value = true
+      const resultado = await areaNavService.getCombo(JSON.stringify({ id_municipio: novo2 }))
+      if (resultado.error) {
+        areas_nav.value = []
+      } else {
+        areas_nav.value = await resultado
+      }
+    } else {
+      navs.value = false
+    }
+  } else if (novo1) {
+    navs.value = novo1 == 6 || novo1 == 7
+  } else {
+    areas_nav.value = []
+  }
+})
 
 async function imoveis() {
   v$.value.$touch()
@@ -420,6 +487,19 @@ watch(
   () => vc_linha.id_censitario,
   async (val) => {
     const result = await quarteiraoService.getCombo(JSON.stringify({ id_censitario: val }))
+    if (result.error) {
+      console.log(result.error)
+      quarteiraos.value = []
+    } else {
+      quarteiraos.value = result
+    }
+  }
+)
+
+watch(
+  () => vc_linha.id_area_nav,
+  async (val) => {
+    const result = await areaNavService.getComboQuart(val)
     if (result.error) {
       console.log(result.error)
       quarteiraos.value = []
